d7cb39a132d09d2486c3c0ddaab3ee03cdd44cce
joelwembo <joelotepawembo@gmail.com>
2021-06-05 20:00:45 +0800

    Django Rest Api athentification

--- django-program/backend/backend/urls.py
+++ django-program/backend/backend/urls.py
@@ -1,7 +1,6 @@
 """backend URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+The `urlpatterns` list routes URLs to views.
 Examples:
 Function views
     1. Add an import:  from my_app import views
